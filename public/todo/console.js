047c1aee8dbca1589b45773e960fe2fe933badd0
Marcin Szczepanski <mszczepanski@atlassian.com>
2017-04-06 08:22:37 +1000

    Improve console

--- public/todo/console.js
+++ public/todo/console.js
@@ -19,10 +19,11 @@ Console.prototype.render = function () {
     this.container.hidden = false;
     this.container.left = window.innerWidth - CONSOLE_WIDTH;
 
+    this.console.document.write('<body text=FFFFFF><table bgcolor=000000 cellpadding=5><tr><td>');
     for (var i = 0; i < this.msgs.length; i++) {
-        this.console.document.write("<p>" + this.msgs[i] + "<br>");
+        this.console.document.write("<p><font face='monospace'>" + this.msgs[i]);
     }
-
+    this.console.document.write('</td></tr></table>');
     this.console.document.close();
 }
 
@@ -34,15 +35,16 @@ Console.prototype.log = function (msg) {
     this.render();
 }
 
-Console.prototype.hide = function () {
-    this.container.hidden = true;
+Console.prototype.toggle = function () {
+    this.console.hidden = !this.console.hidden;
 }
 
 window.console = new Console('console-content');
 
 document.write('<layer id="console-container">' +
-    '<table width=' + CONSOLE_WIDTH + ' height=100% border=1 cellpadding=0 cellspacing=0>' +
-        '<tr><td valign=top align=right><a href=# onclick="console.hide(); return false;">Hide</a><br>' + 
+    '<table width=' + CONSOLE_WIDTH + ' height=100% border=0 cellpadding=0 cellspacing=0>' +
+        '<tr><td valign=top align=right>' +
+        '<a href=# onclick="console.toggle(); return false;">Toggle console</a><br>' + 
         '<layer id="console-content">?!?</layer></td></tr>' +
     '</table>' +
 '</layer>');
