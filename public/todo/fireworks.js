2cc2bc0db893496c1bc853518123c3b5d7c47a60
Marcin Szczepanski <mszczepanski@atlassian.com>
2017-04-26 17:29:22 +0800

    Fireworks v0.5

--- public/todo/fireworks.js
+++ public/todo/fireworks.js
@@ -1,10 +1,11 @@
 var G = 9.54;
 
 (function (Fireworks) {
-    var Emitter = function (id, x, y) {
+    var Emitter = function (id, x, y, direction) {
         this.id = id;
         this.x = x;
         this.y = y;
+        this.direction = direction;
 
         this.size = {
             h: document.height || document.body.scrollHeight,
@@ -21,29 +22,41 @@ var G = 9.54;
         this.maxParticles = 10;
         this.particles = [];
         this.writeBuffer = [];
+        this.count = 0;
     }
 
-    Emitter.prototype.write = function (i, x, y) {
+    Emitter.prototype.write = function (particle) {
         if (document.layers) {
-            var newLayer = '<layer id="particle-' + i + '" left=' + x + ' top=' + y + ' width=10 height=10>#</layer>';
-            this.writeBuffer.push(newLayer);
+            var existing = document.layers[this.id].document.layers[particle.id];
+            if (false) {
+                this.debug('reusing ' + particle.id);
+                existing.left = particle.x;
+                existing.top = particle.y;
+            } else {
+                this.debug('??');
+                var newLayer = '<layer id="' + particle.id + '" left=' + particle.x +
+                    ' top=' + particle.y + ' width=10 height=10 style="background-color:#' +
+                    particle.c +';color:' + particle.c + '">#</layer>';
+                this.writeBuffer.push(newLayer);
+            }
         } else {
-            var existing = document.getElementById('particle-' + i);
+            var existing = document.getElementById(particle.id);
             if (existing) {
-                existing.style.left = x + 'px';
-                existing.style.top = y + 'px';
+                existing.style.left = particle.x + 'px';
+                existing.style.top = particle.y + 'px';
             } else {
                 var el = document.createElement('div');
                 el.className = 'particle';
-                el.id = "particle-" + i;
-                el.innerHTML = '#';
+                el.id = particle.id;
+                el.innerHTML = '';
+                el.style.backgroundColor = '#' + particle.c;
                 document.getElementById(this.id).appendChild(el);
             }
         }
     }
 
     Emitter.prototype.flush = function () {
-        if (!document.layers) {
+        if (!document.layers || this.writeBuffer.length === 0) {
             return;
         }
         var buf = this.writeBuffer.join('');
@@ -53,9 +66,9 @@ var G = 9.54;
         this.writeBuffer = [];
     }
 
-    Emitter.prototype.remove = function (i) {
+    Emitter.prototype.remove = function (particle) {
         if (document.getElementById) {
-            var el = document.getElementById('particle-' + i);
+            var el = document.getElementById(particle.id);
             if (el) {
                 document.getElementById(this.id).removeChild(el);
             }
@@ -73,16 +86,26 @@ var G = 9.54;
 
     }
 
+    function randomColor() {
+        function randomColorPart() {
+            var n = Math.floor(Math.random() * 255);
+            return (n < 10 ? '0' : '') + (new Number(n).toString(16));
+        }
+        return randomColorPart() + randomColorPart() + randomColorPart();
+    }
+
     Emitter.prototype.step = function () {
         var i;
         if (this.particles.length < this.maxParticles) {
             var newCount = this.maxParticles - this.particles.length;
             for (i = 0; i < newCount; i++) {
                 this.particles.push({
+                    id: this.id + (this.count++),
                     x: this.x,
-                    y: this.y,
-                    vx: 10 + (Math.random() * 10),
-                    vy: -20 + (Math.random() * 10)
+                    y: this.y + ((Math.random() * 40) - 20),
+                    vx: (10 + (Math.random() * 10)) * this.direction,
+                    vy: -20 + (Math.random() * 10),
+                    c: randomColor()
                 });
             }
         }
@@ -96,11 +119,11 @@ var G = 9.54;
 
             if (particle.y < this.size.h &&
                 particle.x < this.size.w &&
-                particle.x > 0) {
+                particle.x >= 0) {
                 newParticles.push(particle);
-                this.write(i, particle.x, particle.y);
+                this.write(particle);
             } else {
-                this.remove(i);
+                this.remove(particle);
             }
         }
 
@@ -109,8 +132,8 @@ var G = 9.54;
 
     }
 
-    Fireworks.init = function(id, x, y) {
-        var emitter = new Emitter(id, x, y);
+    Fireworks.init = function(id, x, y, direction) {
+        var emitter = new Emitter(id, x, y, direction);
 
         var timer = setInterval(function () {
             emitter.step()
@@ -125,7 +148,9 @@ var G = 9.54;
 
 })(window.Fireworks = window.Fireworks || {});
 
-var stop = Fireworks.init("fireworks", 0, 300);
+var stop = Fireworks.init("fireworks-left", 0, 300, 1);
+var stop2 = Fireworks.init("fireworks-right", document.width || document.body.scrollWidth, 300, -1);
 setTimeout(function () {
     stop();
+    stop2();
 }, 1000);
